cc0ea6dc4ac8cd6ca00335c96b343a047c224511
afkisch <104489999+afkisch@users.noreply.github.com>
2023-05-14 02:14:34 +0200

    Update aufgabe_1.c

--- aufgabe_1.c
+++ aufgabe_1.c
@@ -75,14 +75,14 @@ void adcIntHandler (void){
    if(idx+1==BUFFER_SIZE) idx = 0; else idx++;
 
    GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, 0x00);
-   if (sum > 5) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0, 0xFF);
-   if (sum > 25) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_1, 0xFF);
-   if (sum > 125) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_2, 0xFF);
-   if (sum > 625) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_3, 0xFF);
-   if (sum > 3125) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4, 0xFF);
-   if (sum > 15625) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_5, 0xFF);
-   if (sum > 78125) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_6, 0xFF);
-   if (sum > 390625) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_7, 0xFF);
+   if(sum > 5) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0, 0xFF);
+   if(sum > 25) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_1, 0xFF);
+   if(sum > 125) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_2, 0xFF);
+   if(sum > 625) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_3, 0xFF);
+   if(sum > 3125) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4, 0xFF);
+   if(sum > 15625) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_5, 0xFF);
+   if(sum > 78125) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_6, 0xFF);
+   if(sum > 390625) GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_7, 0xFF);
 
    // am Ende von adcIntHandler, Interrupt-Flag loeschen
    ADCIntClear(ADC0_BASE,3);
